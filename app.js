914e6916a9cb325ac94f3771a91bf076a53bef78
conba <74415729+conbaDEV@users.noreply.github.com>
2021-05-12 15:02:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+function basketball(){
+    document.getElementById('futbol').style.display = "none";
+    document.getElementById('basketbol').style.display = "block";
+    document.getElementById('bgimg').style.display = "none";
+    
+}
+function football(){
+    document.getElementById('basketbol').style.display = "none";
+    document.getElementById('futbol').style.display = "block";
+    document.getElementById('bgimg').style.display = "none";
+}
+
+
+
+function futbolVideo(){
+
+}
